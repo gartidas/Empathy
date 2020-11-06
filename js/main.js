739efb1ee16dcbff58bc7a03fcf3fef1cbe5cbfe
Adam <garamone@gmail.com>
2020-11-06 23:53:24 +0100

    Changed register to login + added register

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
 /*Variables*/
-let btn = document.getElementById('register-btn');
+let btn = document.getElementById('login-btn');
 let overlay = document.getElementById('overlay');
+let username = document.getElementById('username');
+let password = document.getElementById('password');
 
 btn.addEventListener('click', () => {
+    var usernameValue = username.value;
+    var passwordValue = password.value;
 
-    overlay.style.display = 'grid';
-    overlay.classList.add('animation-overlay')
+    if (usernameValue && passwordValue) {
+        overlay.style.display = 'grid';
+        overlay.classList.add('animation-overlay')
 
-    setTimeout(() => {
+        setTimeout(() => {
 
-        overlay.style.display = 'none';
-        overlay.classList.remove('animation-overlay')
+            overlay.style.display = 'none';
+            overlay.classList.remove('animation-overlay')
 
-    }, 5000);
+        }, 5000);
+    }
 
 }, false)
