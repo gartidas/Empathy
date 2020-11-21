17c29be5ba48f570d44ea6e9ad1cd3f3b3ff568c
Adam <garamone@gmail.com>
2020-11-21 18:17:37 +0100

    Added fake review generator

--- js/reviews.js
+++ js/reviews.js
@@ -4,6 +4,8 @@ let navbarOverlay = document.getElementById('navbar-overlay');
 let nav = document.getElementById('nav');
 let opened = false;
 
+let content = document.getElementById('content');
+
 showNavBtn.addEventListener('click', () => {
     if (opened) {
         closeNavbar();
@@ -18,6 +20,12 @@ navbarOverlay.addEventListener('click', () => {
         closeNavbar();
     }
 }, false)
+
+window.addEventListener('load', (event) => {
+    getReviewerData();
+    getReviewerData();
+    getReviewerData();
+});
 
 function openNavbar() {
     nav.classList.add('animation-showNavBar')
@@ -35,4 +43,24 @@ function closeNavbar() {
     navbarOverlay.classList.add('animation-hideNavBarOverlay')
     navbarOverlay.style.display = 'none';
     opened = false;
+}
+
+async function getReviewerData() {
+    const userResponse = await fetch('https://randomuser.me/api/');
+    const userJson = await userResponse.json();
+    const name = userJson.results[0].name.first + " " + userJson.results[0].name.last;
+    const picture = userJson.results[0].picture.medium;
+
+    const reviewResponse = await fetch('http://www.randomtext.me/api/gibberish/p-1/20-38');
+    const reviewJson = await reviewResponse.json();
+    const reviewMessage = reviewJson.text_out;
+
+    createReview({ name, picture, reviewMessage });
+}
+
+function createReview(reviewerData) {
+    const review = document.createElement('div');
+    review.classList.add('review');
+    review.innerHTML = `<div class="reviewer" id="reviewer"><img src="${reviewerData.picture}" alt="Reviewers profile photo" class="reviewer-photo" id="reviewer-photo"><h3 class="reviewer-name" id="reviewer-name">${reviewerData.name}</h3></div><p class="lorem sm">${reviewerData.reviewMessage}</p>`;
+    content.appendChild(review);
 }
